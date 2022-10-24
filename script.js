6d77fa0424dde55aac294859f6cbd82ca0e8e474
XRichardroidX <oguzieibehrichard@gmail.com>
2022-10-24 18:16:34 +0100

    Add some features to script.js file

--- script.js
+++ script.js
@@ -2,38 +2,40 @@ const mainStack = document.querySelector('.main');
 const title = document.getElementById('title');
 const author = document.getElementById('author');
 const addButton = document.querySelector('.Add-BTN');
+let StoredData;
+let counter = -1;
+let bookListStack = [];
 
-let bookListStack = [
-  {
-    Title: "Lorem ipsum",
-    Author: "Testeroo Tesryy"
-  },
-  {
-    Title: "Lorem ipsum",
-    Author: "Testeroo Tesryy"
-  },
-  {
-    Title: "Lorem ipsum",
-    Author: "Testeroo Tesryy"
-  },
-];
+addButton.addEventListener('click',
+   () => {
+     bookListStack.unshift({
+        Title: title.value,
+        Author: author.value
+    });
+       localStorage.setItem("BooksLocalStorage" ,JSON.stringify(bookListStack));
+       setInterval(function(){
+        location.reload()}, 100);
+   }
+);
+  StoredData = JSON.parse(localStorage.getItem("BooksLocalStorage"));
+  bookListStack = StoredData;
 
-let i
 bookListStack.forEach((element) => {
+    counter++;
     const card = `<ul class="book-list">
     <li class="title">${element.Title}</li>
     <li class="author">${element.Author}</li>
-    <li><button onclick="functionX()">Remove</button></li>
+    <li><button onClick="RemoveBook(${counter})">Remove</button></li>
     <hr>
 </ul>`;
 mainStack.insertAdjacentHTML('afterbegin', card);
 
 });
 
-addButton.addEventListener('click', ()=>{
-    bookListStack.push({
-        Title: "Lorem ipsum",
-        Author: "Testeroo Tesryy"
-    });
-    console.log(bookListStack);
-});
+function RemoveBook(index){
+    bookListStack.splice(index, 1);
+    localStorage.setItem("BooksLocalStorage", JSON.stringify(bookListStack));
+    console.log(index);
+    setInterval(function(){
+    location.reload()}, 100);
+}
